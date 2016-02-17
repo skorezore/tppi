347dfa312fd8da9de57a7e934cf728df46366a76
Julian Torreno <Skorezore@gmail.com>
2016-02-17 01:30:32 -0500

    Add semicolon.

--- include/tppi/pods.hpp
+++ include/tppi/pods.hpp
@@ -19,7 +19,7 @@
 namespace tppi {
     struct vec2 { double x, y; };
     struct vec3 { double x, y, z; };
-    struct vec4 { double x, y, z, w; }
+    struct vec4 { double x, y, z, w; };
 
     struct material_t {
         std::string name;
